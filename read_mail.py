b7b729043ec7cefe7536d5fa5383604ee00c790e
Rahul Sanoria <39688195+rahulsanoria@users.noreply.github.com>
2019-06-24 16:56:06 +0530

    Add files via upload

--- read_mail.py
+++ read_mail.py
@@ -6,9 +6,10 @@ import mimetypes
 import csv
 import io, json
 import pandas as pd
+import fileinput
 
-username = 'your_email_id'
-password = 'password'
+username = 'rahul.kumar25011999@gmail.com'
+password = '07sainty'
 
 mail = imaplib.IMAP4_SSL("imap.gmail.com") 
 mail.login(username, password)
@@ -21,11 +22,12 @@ inbox_item_list = data[0].split()
 result2, email_data = mail.uid('fetch', inbox_item_list[-1], '(RFC822)')
 raw_email = email_data[0][1]
 email_message = email.message_from_string(raw_email)
+
+message_id = email_message['Message-ID']
 to_ = email_message['To']
 from_ = email_message['From']
 subject_ = email_message['Subject']
 date_ = email_message['date']
-
 counter = 1
 
 for part in email_message.walk():
@@ -54,12 +56,19 @@ elif "html" in content_type:
 else:
 	body = content_type
 
-x = {"FROM" : from_ , "TO" : to_ , "DATE" : date_ , "SUBJECT" : subject_ , "BODY" : body }
-b = json.dumps(x)
-a = json.loads(b)
-f = csv.writer(open("append.csv", "a"))
-# f.writerow(["FROM", "TO", "DATE", "SUBJECT", "BODY"])
-f.writerow([a["FROM"] , a["TO"] , a["DATE"] , a["SUBJECT"] , a["BODY"]])
+flag = True
+with open('append.csv','r')as f:
+    data = csv.reader(f)
+    for row in data:
+        if message_id == row[0]:
+            flag = False
+            break
+if flag == True:
+    x = {"Message-ID" : message_id , "FROM" : from_ , "TO" : to_ , "DATE" : date_ , "SUBJECT" : subject_ , "BODY" : body }
+    b = json.dumps(x)
+    a = json.loads(b)
+    f = csv.writer(open("append.csv", "a"))
+    f.writerow([a["Message-ID"],a["FROM"] , a["TO"] , a["DATE"] , a["SUBJECT"] , a["BODY"]])
 
 save_path = os.path.join(os.getcwd(), "Email_attachment", date_, subject_)
 if not os.path.exists(save_path):
